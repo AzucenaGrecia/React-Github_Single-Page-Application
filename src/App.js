a28d9a3c591f6391516cb84245e9b12911ec6388
Robert <robert.tm2018@gmail.com>
2021-03-05 14:02:11 -0500

    Maked navbar component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Button from './Components/UI/button';
 import { Content, ContentLargeBold, ContentSmall } from './Components/Texts/Content';
 import { Card } from './Components/Containers/Card'
 import Input from './Components/Forms/Input';
+import Navbar from './Components/Containers/Navbar';
 
 function App() {
   return (
@@ -33,6 +34,14 @@ function App() {
       <ContentSmall>sdjfhkjdhjfkh</ContentSmall>
       <Card></Card>
       <Input></Input>
+
+      <Navbar
+        css={css`
+          position: fixed;
+          bottom: 0;
+          width: 100%;
+        `}
+      />
     </div>
   );
 }
---
+++ src/Components/Containers/Navbar.js
@@ -0,0 +1,49 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+/* import { Link, useLocation } from "react-router-dom"; */
+import Icon from "../UI/icon";
+
+const StyledDiv = styled.div`
+  height: 66px;
+  padding: 8px 0;
+  display: flex;
+  justify-content: center;
+  background: #f2f2f2;
+  box-shadow: 0px -2px 0px rgba(0, 0, 0, 0.25);
+  .icons-container {
+    width: 250px;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+function Navbar({ css }) {
+/*   const location = useLocation(); */
+/*   const selected = location.pathname; */
+  const selected = "/";
+  const selectedOptions = {
+    home: "/",
+    search: "/search",
+    star: "/favorites",
+  };
+  const icons = ["home", "search", "star"];
+  return (
+    <StyledDiv css={css}>
+      <div className="icons-container">
+        {icons.map((icon, index) => (
+
+            <Icon
+              key={index}
+              type={icon}
+              fill={selected === selectedOptions[icon] ? "#828282" : "#BDBDBD"}
+              size={50}
+            />
+
+        ))}
+      </div>
+    </StyledDiv>
+  );
+}
+
+export default Navbar;
