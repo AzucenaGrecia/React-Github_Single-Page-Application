7cbfd85df6896764fd04e15ba3a9d234aa336e70
Grecia Delgado <greciadm2596@gmail.com>
2021-03-05 19:38:14 -0500

    Add page Repos

--- src/Components/Containers/ReṕoResume.js
+++ src/Components/Containers/ReṕoResume.js
@@ -6,12 +6,14 @@ const StyledCardFooter = styled.div`
   display:flex;
   align-items:center;
   margin-top:4px;
+  background: transparent;
 
   & .card_footer_section{
     display:flex;
     align-items:center;
     margin-right: 16px;
     gap: 4px;
+    background: transparent;
   }
 
 `
